09a077c47eb39a3dfed5d806d6ad9bcddc525c07
Abdulla Ouda <aouda6@uwo.ca>
2017-11-12 13:37:47 -0500

    Comments + Clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,12 @@ import { ContactPage } from '../pages/contact/contact';
 import { PostsPage } from '../pages/posts/posts';
 
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
-import { AccordionComponent } from '../components/accordion/accordion';
-import { DeletePostComponent } from '../components/delete-post/delete-post';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Information retrieved from Firebase database created.
 var config = {
   apiKey: "AIzaSyAqj0Dl6TTVlBJ37xiq36B77qkFVbK47xU",
   authDomain: "blogdb-58d1b.firebaseapp.com",
@@ -30,8 +29,6 @@ var config = {
     HomePage,
     ContactPage,
     PostsPage,
-    AccordionComponent,
-    DeletePostComponent
   ],
   imports: [
     BrowserModule,
--- src/pages/posts/posts.ts
+++ src/pages/posts/posts.ts
@@ -12,29 +12,34 @@ export class PostsPage implements OnInit {
 
   posts: Observable<any[]>;
   keyNames: string[];
-  postData: Object[];
+  dataModel: Object[];
 
   constructor(public navCtrl: NavController,
               public alertCtrl: AlertController, public fdb: AngularFireDatabase) {
 
+    // This observes any changes of the data in the database.
     this.posts = fdb.list('posts').valueChanges();
   }
 
+  // This runs at the beginning of the component lifecycle.
   ngOnInit() {
     this.fdb.object('posts').valueChanges().forEach(rec => {
+      // Upload the records from the database into the model. This includes
+      // an array of keys and array of corresponding data.
       if (rec) {
         this.keyNames = new Array(Object.keys(rec).length);
         this.keyNames = Object.keys(rec);
 
-        this.postData = new Array(Object.keys(rec).length);
+        this.dataModel = new Array(Object.keys(rec).length);
         for (var i = 0; i < Object.keys(rec).length; i++) {
-          this.postData[i] = (rec[this.keyNames[i]]);
+          this.dataModel[i] = (rec[this.keyNames[i]]);
         }
-
       }
     });
   }
 
+  // A method to add a new blog post to the database. It uses ionic alert component to capture
+  // user input.
   public addItem() {
     let alert = this.alertCtrl.create({
       title: "Add New Post",
@@ -55,6 +60,7 @@ export class PostsPage implements OnInit {
         },
         {
           text: "Post",
+          // Connects and pushes data to the database.
           handler: data => {
             this.fdb.list('posts').push({
               title: data.title,
@@ -67,23 +73,27 @@ export class PostsPage implements OnInit {
     alert.present();
   }
 
+  // A method that deletes one individual record based on the record number i,
+  // by which we get the primary key from the previously saved keys array.
   deletePost(i) {
     this.fdb.object('posts/' + this.keyNames[i]).remove();
   }
 
+  // A method that modifies one individual blog post on the record number i
+  // and saves it back to the database.
   editPost(i) {
-    let onePost: Object = this.postData[i];
+    let onePost: Object = this.dataModel[i];
     let alert = this.alertCtrl.create({
       title: "Edit Post",
       message: "Edit your post",
       inputs: [
         {
           name: "title",
-          value: onePost['title']
+          value: onePost['title'] // Shows the existing title from the data model
         },
         {
           name: "body",
-          value: onePost['body']
+          value: onePost['body'] // Shows the existing body from the data model
         }
       ],
       buttons: [
@@ -93,6 +103,7 @@ export class PostsPage implements OnInit {
         {
           text: "Save",
           handler: data => {
+            // Connects and saves the new data to the database
             this.fdb.object('posts/' + this.keyNames[i]).update({
               title: data.title,
               body: data.body
@@ -102,7 +113,7 @@ export class PostsPage implements OnInit {
       ]
     });
     alert.present();
-
   }
+
 }
 
